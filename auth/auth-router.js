a15ef2b97bd3078a46ff6011ed7780fbc66d6937
Rasha Moumneh <rasha@fastmail.com>
2019-12-19 18:24:51 -0500

    fix validate registration middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -13,7 +13,7 @@ router.post("/register", validateRegistration, (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      res.status(201).json(user);
     })
     .catch(error => {
       res.status(500).json("NO NO");
--- middleware/validateRegistration.js
+++ middleware/validateRegistration.js
@@ -2,22 +2,23 @@ const Users = require("../users/users-model");
 
 module.exports = (req, res, next) => {
   const { username, password, department } = req.body;
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (!username || !password || !department) {
-        res
-          .status(400)
-          .json("Please provide a username, password, and department");
-      } else if (user.username !== username) {
-        next();
-      } else {
-        res
-          .status(400)
-          .json("That username already exists. Please try another");
-      }
-    })
-    .catch(error => {
-      res.status(500).json("Error registering user");
-    });
+  if (!username || !password || !department) {
+    res.status(400).json("Please provide a username, password, and department");
+  } else {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        console.log(user);
+        if (user) {
+          res
+            .status(400)
+            .json("That username already exists. Please try another");
+        } else {
+          next();
+        }
+      })
+      .catch(error => {
+        res.status(500).json("Error registering user");
+      });
+  }
 };
